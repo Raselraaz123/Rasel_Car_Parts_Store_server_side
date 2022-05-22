19d3316887ad2195b2dc255a1f5b0cb8f78eab44
Raselraaz123 <raselraaz91@gmail.com>
2022-05-22 22:39:40 +0600

    heroku deploy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -11,11 +11,6 @@ const port = process.env.PROT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.0h0zm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -23,30 +18,27 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
 async function run() {
   try {
     await client.connect();
-    const productCollection = client.db("Rasel_Car_Parts_Store").collection("product");
-    
+    const productCollection = client
+      .db("Rasel_Car_Parts_Store")
+      .collection("product");
 
-    app.get('/product', async (req, res) => {
+    app.get("/product", async (req, res) => {
       const query = {};
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
-      res.send(products)
-    })
+      res.send(products);
+    });
   } finally {
- 
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('The server side is open')
+app.get("/", (req, res) => {
+  res.send("The server side is open");
 });
 app.listen(port, () => {
-  console.log(`The manufacturer web is running ${port}`)
-})
+  console.log(`The manufacturer web is running ${port}`);
+});
