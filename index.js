f9a864551e498a2b18c2803bedce435fae2e9d90
Raselraaz123 <raselraaz91@gmail.com>
2022-05-27 16:24:33 +0600

    product recived by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -54,6 +54,15 @@ async function run() {
       const products = await cursor.toArray();
       res.send(products);
     });
+    
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+})
+
+
     app.get('/review', async (req, res) =>{
       const query = {};
       const cursor = reviewCollection.find(query);
